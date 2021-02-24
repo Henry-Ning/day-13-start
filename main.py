c16e4b8e1fd4bbffcfef4bac1b5902be7727af15
Henry Ning <henryhuning@gmail.com>
2021-02-24 10:31:15 +0000

    128. [Interactive Coding Exercise] Debugging Odd or Even 3min

--- main.py
+++ main.py
@@ -38,11 +38,19 @@
 # print(total_words)
 
 #Use a Debugger
-def mutate(a_list):
-  b_list = []
-  for item in a_list:
-    new_item = item * 2
-    b_list.append(new_item)
-  print(b_list)
+# def mutate(a_list):
+#   b_list = []
+#   for item in a_list:
+#     new_item = item * 2
+#     b_list.append(new_item)
+#   print(b_list)
 
-mutate([1,2,3,5,8,13])
+# mutate([1,2,3,5,8,13])
+
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+  
