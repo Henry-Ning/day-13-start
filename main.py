e21e535e56401068d92a45e2e9bbe376db4ad607
Henry Ning <henryhuning@gmail.com>
2021-02-28 13:55:09 +0000

    133. Solution & Walkthrough of the Higher Lower Game 31min

--- main.py
+++ main.py
@@ -1,79 +1,83 @@
-#2-18
+from game_data import data
+import random
+from art import logo, vs
+from replit import clear
 
-############DEBUGGING#####################
+def get_random_account():
+  """Get data from random account"""
+  return random.choice(data)
 
-# # Describe Problem
-# def my_function():
-#   for i in range(1, 21):
-#     if i == 20:
-#       print("You got it")
-# my_function()
+def format_data(account):
+  """Format account into printable format: name, description and country"""
+  name = account["name"]
+  description = account["description"]
+  country = account["country"]
+  # print(f'{name}: {account["follower_count"]}')
+  return f"{name}, a {description}, from {country}"
 
-# # Reproduce the Bug
-# from random import randint
-# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(0, 5)
-# print(dice_imgs[dice_num])
-
-# # Play Computer
-# year = int(input("What's your year of birth? "))
-# if year > 1980 and year < 1994:
-#   print("You are a millenial.")
-# elif year >= 1994:
-#   print("You are a Gen Z.")
-
-# # Fix the Errors
-# age = int(input("How old are you?"))
-# if age > 18:
-#   print(f"You can drive at age {age}.")
-
-#Print is Your Friend
-# pages = 0
-# word_per_page = 0
-# pages = int(input("Number of pages: "))
-# word_per_page = int(input("Number of words per page: "))
-# total_words = pages * word_per_page
-# print(pages)
-# print(word_per_page)
-# print(total_words)
-
-#Use a Debugger
-# def mutate(a_list):
-#   b_list = []
-#   for item in a_list:
-#     new_item = item * 2
-#     b_list.append(new_item)
-#   print(b_list)
-
-# mutate([1,2,3,5,8,13])
-
-# number = int(input("Which number do you want to check?"))
-
-# if number % 2 == 0:
-#   print("This is an even number.")
-# else:
-#   print("This is an odd number.")
-  
-# year = int(input("Which year do you want to check?"))
-
-# if year % 4 == 0:
-#   if year % 100 == 0:
-#     if year % 400 == 0:
-#       print("Leap year.")
-#     else:
-#       print("Not leap year.")
-#   else:
-#     print("Leap year.")
-# else:
-#   print("Not leap year.")
-  
-
-for number in range(1, 101):
-  if number % 3 == 0 and number % 5 == 0:
-    print("FizzBuzz")
-  elif number % 3 == 0:
-    print("Fizz")
-  elif number % 5 == 0:
-    print("Buzz")
+def check_answer(guess, a_followers, b_followers):
+  """Checks followers against user's guess 
+  and returns True if they got it right.
+  Or False if they got it wrong.""" 
+  if a_followers > b_followers:
+    return guess == "a"
   else:
-    print([number])
+    return guess == "b"
+
+
+def game():
+  print(logo)
+  score = 0
+  game_should_continue = True
+  account_a = get_random_account()
+  account_b = get_random_account()
+
+  while game_should_continue:
+    account_a = account_b
+    account_b = get_random_account()
+
+    while account_a == account_b:
+      account_b = get_random_account()
+
+    print(f"Compare A: {format_data(account_a)}.")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}.")
+    
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    clear()
+    print(logo)
+    if is_correct:
+      score += 1
+      print(f"You're right! Current score: {score}.")
+    else:
+      game_should_continue = False
+      print(f"Sorry, that's wrong. Final score: {score}")
+
+game()
+
+
+# Generate a random account from the game data.
+
+# Format account data into printable format.
+
+# Ask user for a guess.
+
+# Check if user is correct.
+## Get follower count.
+## If Statement
+
+# Feedback.
+
+# Score Keeping.
+
+# Make game repeatable.
+
+# Make B become the next A.
+
+# Add art.
+
+# Clear screen between rounds.
